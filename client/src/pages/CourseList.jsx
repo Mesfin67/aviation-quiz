6227a7a28510798df67ce2dea55c21a4e3dfbd10
Mesfin67 <mesfinbebulo@gmail.com>
2025-05-06 17:57:58 +0300

    Optimize quiz app

--- client/src/pages/CourseList.jsx
+++ client/src/pages/CourseList.jsx
@@ -9,50 +9,50 @@ const CourseList = ({ collapseNav }) => {
   // New sub-courses details for each main course.
   const subCoursesMapping = {
     GC: [
-      { subNum: "GC001", quizTime: "60 min", Numberofquestions: "30", topics: ["Aviation Mathematics"] },
-      { subNum: "GC002", quizTime: "60 min", Numberofquestions: "30", topics: ["Aviation Physics"] },
-      { subNum: "GC003", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Drawing"] },
-      { subNum: "GC004", quizTime: "60 min", Numberofquestions: "30", topics: ["Basic Aerodynamics"] },
-      { subNum: "GC005", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Materials and Hardware"] },
-      { subNum: "GC006", quizTime: "60 min", Numberofquestions: "30", topics: ["Workshop Practices"] },
-      { subNum: "GC007", quizTime: "60 min",  Numberofquestions: "30", topics: ["Avionics, Airframe and Powerplant Familiarization"] },
+      { subNum: "GC001", quizTime: "60 min", Numberofquestions: "20", topics: ["Aviation Mathematics"] },
+      { subNum: "GC002", quizTime: "60 min", Numberofquestions: "20", topics: ["Aviation Physics"] },
+      { subNum: "GC003", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Drawing"] },
+      { subNum: "GC004", quizTime: "60 min", Numberofquestions: "20", topics: ["Basic Aerodynamics"] },
+      { subNum: "GC005", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Materials and Hardware"] },
+      { subNum: "GC006", quizTime: "60 min", Numberofquestions: "20", topics: ["Workshop Practices"] },
+      { subNum: "GC007", quizTime: "60 min",  Numberofquestions: "20", topics: ["Avionics, Airframe and Powerplant Familiarization"] },
       { subNum: ": GC Final", quizTime: "2hr", Numberofquestions: "50", topics: ["General Courses"] }
 
     ],
     AV: [
-      { subNum: "AV001", quizTime: "60 min", Numberofquestions: "30", topics: ["Electrical Fundamentals"] },
-      { subNum: "AV002", quizTime: "60 min", Numberofquestions: "30", topics: ["Electronics Fundamentals"] },
-      { subNum: "AV003", quizTime: "60 min", Numberofquestions: "30", topics: ["Digital Techniques and Computers"] },
-      { subNum: "AV004", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Electrical Systems 1"] },
-      { subNum: "AV005", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Electrical Systems 2"] },
-      { subNum: "AV006", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Communication and Navigation System"] },
-      { subNum: "AV007", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Instrument Systems"] },
+      { subNum: "AV001", quizTime: "60 min", Numberofquestions: "20", topics: ["Electrical Fundamentals"] },
+      { subNum: "AV002", quizTime: "60 min", Numberofquestions: "20", topics: ["Electronics Fundamentals"] },
+      { subNum: "AV003", quizTime: "60 min", Numberofquestions: "20", topics: ["Digital Techniques and Computers"] },
+      { subNum: "AV004", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Electrical Systems 1"] },
+      { subNum: "AV005", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Electrical Systems 2"] },
+      { subNum: "AV006", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Communication and Navigation System"] },
+      { subNum: "AV007", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Instrument Systems"] },
       { subNum: "Avionics Final", quizTime: "2hr", Numberofquestions: "50", topics: ["Avionics Courses"] }
 
     ],
     AF: [
-      { subNum: "AF001", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Structure, Assembly, Rigging and Weight & Balance"] },
-      { subNum: "AF002", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Structural Materials and Repairs"] },
-      { subNum: "AF003", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Pneumatic and Hydraulic Systems"] },
-      { subNum: "AF004", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Landing Gear and Flight Control Systems"] },
-      { subNum: "AF005", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Fuel System"] },
-      { subNum: "AF006", quizTime: "60 min", Numberofquestions: "30", topics: ["Cabin Environmental System"] },
-      { subNum: "AF007", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Auxiliary System"] },
+      { subNum: "AF001", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Structure, Assembly, Rigging and Weight & Balance"] },
+      { subNum: "AF002", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Structural Materials and Repairs"] },
+      { subNum: "AF003", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Pneumatic and Hydraulic Systems"] },
+      { subNum: "AF004", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Landing Gear and Flight Control Systems"] },
+      { subNum: "AF005", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Fuel System"] },
+      { subNum: "AF006", quizTime: "60 min", Numberofquestions: "20", topics: ["Cabin Environmental System"] },
+      { subNum: "AF007", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Auxiliary System"] },
       { subNum: "Airframe-Final:", quizTime: "2hr", Numberofquestions: "50", topics: ["Airframe Courses"] }
 
     ],
     PP: [
-      { subNum: "PP001", quizTime: "60 min", Numberofquestions: "30", topics: ["Engine Fundamentals"] },
-      { subNum: "PP002", quizTime: "60 min", Numberofquestions: "30", topics: ["Gas Turbine Engine Systems"] },
-      { subNum: "PP003", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Propeller Systems"] },
-      { subNum: "PP004", quizTime: "60 min", Numberofquestions: "30", topics: ["Aircraft Engine Inspection & Maintenance"] },
+      { subNum: "PP001", quizTime: "60 min", Numberofquestions: "20", topics: ["Engine Fundamentals"] },
+      { subNum: "PP002", quizTime: "60 min", Numberofquestions: "20", topics: ["Gas Turbine Engine Systems"] },
+      { subNum: "PP003", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Propeller Systems"] },
+      { subNum: "PP004", quizTime: "60 min", Numberofquestions: "20", topics: ["Aircraft Engine Inspection & Maintenance"] },
       { subNum: "Power plant-Final:", quizTime: "2hr", Numberofquestions: "50", topics: ["Powerplant Courses"] }
 
     ],
     ICAO: [
-      { subNum: "ICAO001", quizTime: "60 min", Numberofquestions: "30", topics: ["ICAO Regulation - Initial"] },
-      { subNum: "ICAO002", quizTime: "60 min", Numberofquestions: "30", topics: ["ET-CAA Regulation"] },
-      { subNum: "ICAO003", quizTime: "60 min", Numberofquestions: "30", topics: ["FAA Regulation"] }
+      { subNum: "ICAO001", quizTime: "60 min", Numberofquestions: "20", topics: ["ICAO Regulation - Initial"] },
+      { subNum: "ICAO002", quizTime: "60 min", Numberofquestions: "20", topics: ["ET-CAA Regulation"] },
+      { subNum: "ICAO003", quizTime: "60 min", Numberofquestions: "20", topics: ["FAA Regulation"] }
     ]
   };
 
