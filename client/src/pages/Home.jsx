b3b15051a06c9875e7f641d2c7d080a44d04c263
Mesfin67 <140214673+Mesfin67@users.noreply.github.com>
2025-05-07 23:10:30 +0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -105,7 +105,7 @@ const Home = ({ setShowLogin, setShowSignup }) => {
             <div style={outerStyle}>
               <div style={overlayStyle}></div>
               <div style={contentCenterStyle}>
-              <h1 className='display-3' > Welcome to Trainee Technicians-Hub</h1>
+              <h1 className='display-3' >Trainee Technicians-Hub</h1>
 <p>
   Test your skills with questions on Avionics, Airframe, Powerplant and more. Learn as you go and enjoy the journey!
 </p>
